82aa6c04602b91441f386affce2a7a45313a8b25
Nagy Balázs Róbert <nagy.balazs2@vereb.dc>
2020-09-28 08:44:50 +0200

    Add project files.

---
+++ Ora/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ora
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] l = new int[20];
+            for (int i = 0; i < 100; i++)
+            {
+                l[i] = rnd.Next(20, 50);
+
+                Console.Write(l[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
